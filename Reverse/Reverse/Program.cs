a8d0380b3380ed074ccb2590c9709719a9f5e3f9
Jazz-Aka-Amazing <Jazsingh12825@gmail.com>
2014-05-07 09:45:01 -0400

    dgDgsdgzsdfgsdfgdszg

---
+++ Reverse/Reverse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reverse
+{
+    class Program
+    {   
+        string output = "";
+        public static string Reverse( string input)
+        {
+            
+            char[] seq = input.ToCharArray();
+            Array.Reverse(seq);
+            string final = new string(seq);
+            input = final;
+            return input;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string:");
+
+            string input = Console.ReadLine();
+            string output = Reverse(input);
+
+            Console.WriteLine(output);
+           
+
+
+            
+           
+        }
+    }
+}
